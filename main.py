2f5551878b7b509406e79d39ef3b3b57e032a436
AmirHosseinRasti <153931968+Boyyey@users.noreply.github.com>
2026-02-09 11:35:48 +0400

    Create main.py to run Cat vs Dog Classifier

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+from cat_dog_classifier import *
+
+print("Running Cat vs Dog Classifier...")
+# All code is already executed in cat_dog_classifier.py
+# This file can simply import and run everything
